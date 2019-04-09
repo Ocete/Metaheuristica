d145b4e68b1d47d515f4a1d2160b0afafa0d36a3
Ocete <ocetebt@hotmail.com>
2019-04-09 19:07:06 +0200

    localSearch con estructura binaria completada

--- P2/src/localSearch.cpp
+++ P2/src/localSearch.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
 #include <set>
+#include <unordered_set>
 #include <stdlib.h>     // srand y rand
 #include <algorithm>    // sort
 #include <time.h>
 
 using namespace std;
 
+vector<vector<double> > MAT;
+
 /////////////////// INPUT //////////////////////
 
-void readInput (vector<vector<double> > &mat) {
+void readInput ( int size ) {
+  vector<double> v (size, 0);
+  vector<vector<double > > mat (size, v);
+  MAT = mat;
   unsigned i, j;
   double f;
-  for (i=1; i<mat.size(); i++) {
-    for (j=i+1; j<mat.size(); j++) {
+  for (i=1; i<MAT.size(); i++) {
+    for (j=i+1; j<MAT.size(); j++) {
       cin >> f >> f >> f;
-      mat[i][j] = mat[j][i] = f;
+      MAT[i][j] = MAT[j][i] = f;
     }
   }
 }
@@ -55,61 +60,64 @@ void printVector (vector<T> &v) {
 // Computes the contribution of the element "elem" for the solution "sol" given
 // AKA, the sum of the distances from elemet "elem" to each other element in sol
 
-template <class T>
-double singleContribution(T &container, vector<vector<double> > &mat, int elem) {
+double singleContribution(vector<bool> &v, int elem) {
   double result = 0;
-  typename T::iterator it;
-  for (it = container.begin(); it != container.end(); it++) {
-    result += mat[ elem ][ *it ];
+  for (unsigned i=0; i<v.size(); i++) {
+    if ( v[i] ) {
+      result += v[i] * MAT[ elem ][ i ];
+    }
   }
   return result;
 }
 
 // Returns the fitness of the whole solution
-template <class T>
-double evaluateSolution(T &container, vector<vector<double> > &mat) {
+double evaluateContainer(vector<bool> &v) {
   double fitness = 0;
-  typename T::iterator it;
-  for (it = container.begin(); it != container.end(); it++) {
-    fitness += singleContribution(container, mat, *it);
+  for (unsigned i=0; i<v.size(); i++) {
+    if ( v[i] ) {
+      fitness += singleContribution(v, i);
+    }
   }
   // Counting twice all the possible distances
   return fitness /= 2;
 }
 
-////////////////// LOCAL SEARCH ///////////////////////
+////////////////// GENETIC ///////////////////////
 
 struct solution {
-  vector<int> v;
+  vector<bool> v;
   double fitness;
+  bool evaluated;
 };
 
-double updateSolution(solution &sol, vector<vector<double> > &mat) {
-  sol.fitness = evaluateSolution(sol.v, mat);
+// Returns a random int in [a,b)
+int random(int a, int b) {
+  return a + rand() % b;
+}
+
+double evaluateSolution(solution &sol) {
+  sol.fitness = evaluateContainer(sol.v);
+  sol.evaluated = true;
   return sol.fitness;
 }
 
 // Creates a random solution
 // Prec.: Random seed already set
-void randomSolution (solution &sol, int size, int choosen) {
-  int currently_choosen = 0, random;
-  unordered_set<int> s;
+void randomSolution (solution &sol, int choosen) {
+  int size = MAT.size();
+  int currently_choosen = 0, rand_n;
+
+  // Set the flag and clear the solution
+  sol.evaluated = false;
+  sol.v = vector<bool> (size, false);
 
   // Select 'choosen' elements
   while (currently_choosen  < choosen) {
-    random = rand() % size;
-    if ( s.find(random) == s.end() ) {
-      s.insert(random);
+    rand_n = random(0, size);
+    if ( !sol.v[rand_n] ) {
+      sol.v[rand_n] = true;
       currently_choosen++;
     }
-  }
-
-  // Dump the set into the solution
-  int i = 0;
-  sol.v.resize(choosen);
-  for (auto it = s.begin(); it != s.end(); it++) {
-    sol.v[i] = *it;
-    i++;
   }
 }
 
@@ -120,68 +128,65 @@ bool operator < (const pair<int,double> &p1, const pair<int,double> &p2) {
 }
 
 // Order sol.v by conribution to the solution in ascending order
-void orderSolutionByContribution(solution &sol, vector<vector<double> > &mat ) {
-  pair<int, double> p (0, 0.0);
-  vector< pair<int, double> > pairs_v ( sol.v.size(), p);
+void orderSolutionByContribution ( solution &sol, vector<int> &ordered ) {
+  vector< pair<int, double> > pairs_v;
+  double cont;
 
   // Initialize the auxiliar vector
-  for (unsigned i=0; i< pairs_v.size(); i++) {
-    pairs_v[i].first = sol.v[i];
-    pairs_v[i].second = singleContribution(sol.v, mat, pairs_v[i].first);
+  for (unsigned i=0; i<sol.v.size(); i++) {
+    if ( sol.v[i] ) {
+      cont = singleContribution(sol.v, i);
+      pairs_v.push_back( pair<int,double> (i, cont) );
+    }
   }
 
   // Order the vector by contribution
   sort(pairs_v.begin(), pairs_v.end());
 
   // Save the ordering
+  ordered.resize( pairs_v.size() );
   for (unsigned i=0; i< pairs_v.size(); i++) {
-    sol.v[i] = pairs_v[i].first;
+    ordered[i] = pairs_v[i].first;
   }
 }
 
 // Computes a single step in the exploration, changing "sol"
-bool stepInNeighbourhood (solution &sol, vector<vector<double> > &mat) {
+bool stepInNeighbourhood ( solution &sol, int &tries, int max_tries ) {
   double percentage_studied;
-  unsigned i = 0, j, element_out, total_tries, max_i, max_randoms, k;
+  unsigned i, j, element_out, max_i, max_randoms, k;
   double newContribution, oldContribution;
 
-  orderSolutionByContribution(sol, mat);
+  vector<int> ordered;
+  orderSolutionByContribution(sol, ordered);
 
-  // percentage_studied = 0.1;
-  // total_tries = 50000;
-
-  percentage_studied = 1;
-  total_tries = 1000000;
+  percentage_studied = 0.2;
 
   max_i = max(percentage_studied * sol.v.size(), 1.0);
-  max_randoms = total_tries / max_i;
-
-  // Fill hash with our used values
-  unordered_set<int> s;
-  for (unsigned i=0; i<sol.v.size(); i++) {
-    s.insert( sol.v[i] );
-  }
+  max_randoms = max_tries / max_i;
 
   // Explore the neighbourhood and return the firstly found better option
-  while (i < max_i) {
+  i = 0;
+  while (i < max_i && tries < max_tries) {
     // Save data of the element we are trying to swap
-    element_out = sol.v[i];
-    oldContribution = singleContribution(sol.v, mat, element_out);
+    element_out = ordered[i];
+    oldContribution = singleContribution(sol.v, element_out);
 
     k = 0;
-    j = rand() % mat.size();
-    while (j < mat.size() && k < max_randoms) {
+    j = random(0, MAT.size());
+    while ( k < max_randoms && tries < max_tries ) {
       // Try the swap if the element 'j' is not in the current solution
-      if ( s.find(j) == s.end() ) {
-        newContribution = singleContribution(sol.v, mat, j) - mat[j][element_out];
+      if ( !sol.v[j] ) {
+        newContribution = singleContribution(sol.v, j) - MAT[j][element_out];
+        tries++;
         if ( newContribution > oldContribution ) {
-          sol.v[i] = j;
+          sol.v[element_out] = false;
+          sol.v[j] = true;
           sol.fitness = sol.fitness + newContribution - oldContribution;
           return false;
         }
         k++;
       }
-      j = rand() % mat.size();
+      j = random(0, MAT.size());
     }
     i++;
   }
@@ -189,79 +194,40 @@ bool stepInNeighbourhood (solution &sol, vector<vector<double> > &mat) {
 }
 
 // Computes the local search algorithm for a random starting solution
-double localSearch(vector<vector<double> > &mat, int choosen) {
+double localSearch(int choosen, int &tries, int max_tries) {
   solution sol;
   bool stop = false;
-  int iterations = 0;
   clock_t t_start, t_total;
 
-  // set seed
-  srand (time(NULL));
-
-  randomSolution(sol, mat.size(), choosen);
-  updateSolution(sol, mat);
+  randomSolution(sol, choosen);
+  evaluateSolution(sol);
 
   t_start = clock();
-  while (!stop /*&& iterations < 10000*/) {
-    stop = stepInNeighbourhood(sol, mat);
-    iterations++;
+  while (!stop && tries < max_tries) {
+    stop = stepInNeighbourhood(sol, tries, max_tries);
     // cout << sol.fitness << "\t" << iterations << endl;
   }
   t_total = clock() - t_start;
 
   // output: Fitness - Time - Iterations
-  cout << sol.fitness << "\t" << (double) t_total / CLOCKS_PER_SEC << "\t" << iterations<< endl;
+  cout << sol.fitness << "\t" << (double) t_total / CLOCKS_PER_SEC << "\t" << tries << endl;
   return sol.fitness;
-}
-
-/////////////// TEST ////////////////////
-
-void testEvaluation(vector<vector<double> > &mat, int size, int choosen) {
-  solution sol;
-  for (unsigned i=0; i<10000; i++) {
-    randomSolution(sol, size, choosen);
-    updateSolution(sol, mat);
-    cout << "Iteration " << i << " - fitness " << sol.fitness << endl;
-  }
-}
-
-void testFactorization(vector<vector<double> > &mat, int size, int choosen) {
-  solution sol;
-  double fit_before, fit_after;
-  bool OK = true;
-  for (unsigned i=0; i<100; i++) {
-    randomSolution(sol, size, choosen);
-    updateSolution(sol, mat);
-
-    for (unsigned i=0; i<1000; i++) {
-      stepInNeighbourhood(sol, mat);
-      fit_before = sol.fitness;
-      fit_after = updateSolution(sol,mat);
-
-      if ( abs(fit_after - fit_before) > 0.0001 ) {
-        cerr << "ERROR in stepInNeighbourhood. Fit before: " << fit_before;
-        cerr << " Fit after: " << fit_after << endl;
-        OK = false;
-      }
-    }
-  }
-  if ( OK ) {
-    cout << "No error found in stepInNeighbourhood" << endl;
-  }
 }
 
 //////////////////// MAIN /////////////////////
 
 int main( int argc, char *argv[] ) {
   int size, choosen;
+  int tries = 0, max_tries = 50000;
+
+  // set seed
+  srand (time(NULL));
 
   cin >> size >> choosen;
-  vector<double> v (size, 0);
-  vector<vector<double > > mat (size, v);
-  readInput(mat);
+  readInput(size);
 
   // testEvaluation(mat, size, choosen);
   // testFactorization(mat, size, choosen);
 
-  localSearch(mat, choosen);
+  localSearch(choosen, tries, max_tries);
 }
